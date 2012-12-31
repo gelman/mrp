fd8eacc837bf310d5fd57483c2923ffbaf2c76cc
malecki <mike@crunch.io>
2012-12-31 11:30:30 -0500

    missing states should be omitted from subscripts

--- mrp/R/classMrp.R
+++ mrp/R/classMrp.R
@@ -96,7 +96,7 @@ mrp <- function(formula,
             pop.subscripts <- c(pop.subscripts, addTheseSubscripts)
             pop.dimnames <- c(pop.dimnames, addTheseDimnames)
         }
-        pop.subscripts <- as.matrix(expand.grid(pop.subscripts))
+        pop.subscripts <- na.omit(as.matrix(expand.grid(pop.subscripts)))
         colnames(pop.subscripts) <- names(pop.dimnames)
         pop.array <- array(pop.array,
                            dim=lapply(pop.dimnames, length),
