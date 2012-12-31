a7bd0000861e2300ce36ba18d7af85fc99c9e9d6
malecki <mike@crunch.io>
2012-12-31 15:35:02 -0500

    rearranging

--- mrp/R/mrp.R
+++ mrp/R/mrp.R
@@ -1,28 +1,4 @@
-##' @exportPattern "."
 
-setClass(Class="mrp",
-    representation=representation(
-        poll = "NWayData",
-        data = "data.frame",
-        formula = "formula",
-        multilevelModel = "mer",
-        population = "NWayData"),
-    validity=function (object) {
-      if (is.null (object@data)) {
-        stop ("[mrp: validation] flattened data is missing")
-      }
-      if (is.null (object@poll)) {
-        stop("[mrp: validation] poll NWayData is missing.")
-      }
-      if (is.null (object@population)) {
-        stop("[mrp: validation] pop NWayData is missing.")
-      }
-      if (is.null (object@formula)) {
-        stop("[mrp: validation] formula is missing.")
-      }
-      return(TRUE)
-    }
-)
 
 mrp <- function(formula,
                 data, poll.weights=1,
