32d17b7244d62c9594bc4f40f4f04064c2af9f1d
Michael Malecki <malecki@gmail.com>
2013-03-27 13:25:26 -0400

    mr() arg name and will take other familys now

--- mrp/R/mr.R
+++ mrp/R/mr.R
@@ -2,16 +2,19 @@ setGeneric ("mr", function (object,mr.formula,...) { standardGeneric ("mr")})
 #setGeneric ("multilevelRegression", function (object) { standardGeneric ("multilevelRegression")})
 setMethod (f="mr",
     signature=signature(object="mrp"),
-    definition=function(object,mr.formula,...) {
-      if(missing(mr.formula)) {
+    definition=function(object,mr.formula.update,...) {
+        dots <- list(...)
+        if(is.null(dots$family)) {
+            family <- binomial(link="logit")
+        }
+        if(missing(mr.formula.update)) {
         fm <- object@formula
       } else {
-        fm <- update.formula(object@formula, mr.formula)
+        fm <- update.formula(object@formula, mr.formula.update)
         object@formula <- fm
       }
       response <- as.matrix(getResponse(object))
       object@multilevelModel <- bglmer(fm,
-          data=object@data,
-          family=binomial(link="logit"),...)
+          data=object@data, family=family, ...)
       return (object)
     } )
